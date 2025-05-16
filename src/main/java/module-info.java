5e13c9d03a035dd2eb3406eb660140ff15078506
AngryGrandma <martin.x.krupicka@seznam.cz>
2025-05-16 13:23:26 +0200

    init

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module com.example.dbs_semestralka {
-    opens com.example.dbs_semestralka to javafx.fxml;
-    opens com.example.dbs_semestralka.controllers to javafx.fxml;
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
+    requires org.hibernate.orm.core;    // <— make sure Hibernate’s core jar is on your module path
+    requires java.sql;
+
+    exports com.example.dbs_semestralka;
+
+    opens com.example.dbs_semestralka to javafx.fxml;
+
+    opens com.example.dbs_semestralka.controllers to javafx.fxml;
 }
