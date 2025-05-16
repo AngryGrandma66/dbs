21beed7461d6cd636008fd46d31f88d9590e07a0
AngryGrandma <martin.x.krupicka@seznam.cz>
2025-05-16 13:19:56 +0200

    init

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,7 @@
 module com.example.dbs_semestralka {
+    opens com.example.dbs_semestralka to javafx.fxml;
+    opens com.example.dbs_semestralka.controllers to javafx.fxml;
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
-
-    exports com.example.dbs_semestralka;
-
-    opens com.example.dbs_semestralka to javafx.fxml;
-
-    opens com.example.dbs_semestralka.controllers to javafx.fxml;
 }
